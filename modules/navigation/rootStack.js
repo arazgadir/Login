ea21659e5c67ba06256103d81d03228fdf08dcc9
Araz Gadirov <araz.gadirov@gamil.com>
2022-09-24 22:48:01 +0400

    ..

--- modules/navigation/rootStack.js
+++ modules/navigation/rootStack.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from 'react-navigation/native-stack';
+import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 import { LoginScreen } from '../../screens/loginScreen';
 import { PostScreen } from '../../screens/postScreen';
 
